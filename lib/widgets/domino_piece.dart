6e377534ab050065a3acdcbfdfc6a09e51097dcb
Rafael Dias <rdias.ga@gmail.com>
2020-02-02 02:43:16 -0300

    changes the background color of the dominoes

--- lib/widgets/domino_piece.dart
+++ lib/widgets/domino_piece.dart
@@ -29,7 +29,7 @@ class DominoPiece extends StatelessWidget {
       child: new Container(
         padding: const EdgeInsets.all(8),
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: Color(0xFFEAEBF3),
           borderRadius: BorderRadius.only(
             topLeft: Radius.circular(10),
             topRight: Radius.circular(10),
